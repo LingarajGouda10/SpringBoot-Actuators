b9b1b847ace03b98b09f7fdcdd6742127ffef1c5
LingarajGouda10 <lingarajg@ivoyant.com>
2023-12-07 12:04:22 +0530

    Initial Commit

---
+++ src/test/java/com/ivoyant/SpringBoot/SpringProfiles1/SpringProfiles1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoyant.SpringBoot.SpringProfiles1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProfiles1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
